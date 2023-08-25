cb99e8839bedc0fb01c3d8dda091ff3f254b84fe
viscod <amr.gamal.dev@gmail.com>
2023-08-26 00:35:44 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Create Rust Library for PHP
+
+check the `cargo.toml` file & `build.rs` file
+
+execute this command
+
+`cargo build --release`
+
---
+++ build.rs
@@ -0,0 +1,20 @@
+extern crate cbindgen;
+
+use std::env;
+use cbindgen::Language;
+
+fn main() {
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    cbindgen::Builder::new()
+    .with_crate(crate_dir)
+    .with_language(Language::C)
+    .with_no_includes() // First we strip the default include lines which are not needed
+    .with_header("#define FFI_LIB \"libmerge_pdf.so\"") // then we insert our custom definitions, the .so path is relative to the header file we're writing
+    .generate()
+    .expect("Unable to generate bindings")
+    .write_to_file("target/debug/merge_pdf.h");
+
+}
+
+
